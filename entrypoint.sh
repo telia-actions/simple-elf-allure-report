58d302061ee8d7f3f081191e8d2d3194bf4a0571
Sami Sallmén <sami.sallmen@teliacompany.com>
2022-09-27 13:06:46 +0300

    Fix ENV_NAME environment property value setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,7 +56,7 @@ mv ./executor.json ./${INPUT_ALLURE_RESULTS}
 #environment.properties
 echo "URL=${GITHUB_PAGES_WEBSITE_URL}" >> ./${INPUT_ALLURE_RESULTS}/environment.properties
 if [[ ${INPUT_TEST_ENV_NAME} != '' ]]; then
-  echo "ENV_NAME=test" >> ./${INPUT_ALLURE_RESULTS}/environment.properties
+  echo "ENV_NAME=${INPUT_TEST_ENV_NAME}" >> ./${INPUT_ALLURE_RESULTS}/environment.properties
 fi
 
 echo "keep allure history from ${INPUT_GH_PAGES}/last-history to ${INPUT_ALLURE_RESULTS}/history"
