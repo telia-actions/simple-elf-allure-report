ade3e921c6fee595671a955106dbaf886f2cdd8c
Sami Sallmén <sami.sallmen@teliacompany.com>
2022-11-28 12:58:59 +0200

    Remove old reports based on their git author date - Instead of alphabetical order of folder name i.e. build number

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ if (( COUNT > INPUT_KEEP_REPORTS )); then
   echo "remove index.html last-history"
   rm index.html last-history -rv
   echo "remove old reports"
-  ls | sort -n | head -n -$((${INPUT_KEEP_REPORTS}-2)) | xargs rm -rv;
+  ls **/index.html | xargs -i sh -c 'echo "$(git log -1 --pretty='%ad' --date=format:'%Y-%m-%dT%H:%M:%S' -- {}) {}"' | sort -u | head -n -$((${INPUT_KEEP_REPORTS}-2)) | awk -F '[ /]' '{print $2}' | xargs rm -rv;
   cd ${GITHUB_WORKSPACE}
 fi
 
